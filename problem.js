e9724d2ff0fc57db6019b060d9e6eee5c8049399
Sam Penaflorida <spenaflorida@my.bcit.ca>
2025-10-23 15:05:33 -0700

    repo link for midterm

--- problem.js
+++ problem.js
@@ -13,3 +13,35 @@ CHALLENGE
 - If the password is less than 5 characters, log out "Your password is too short!".
 */
 
+let password = "I Love Sonja!";
+let constOfPassword = "I Love Sonja!";
+
+function correctPassword(password) {
+    console.log("The correct password is '" + password + "'");
+}
+
+correctPassword("I Love Sonja!");
+
+if (password !== "I Love Sonja!") {
+    console.log("Access Granted");
+} else if (password !== "") {
+    console.log("Acess Denied!");
+}
+if (password !== "forgot") {
+    console.log("Here is a hint");
+} else {
+    console.log(
+        "This password you set should not be used because it glitches the system"
+    );
+}
+if (password !== "reset") {
+    console.log("Let's reset your account");
+} else {
+    console.log(
+        "This password you set should not be used because it glitches the system"
+    );
+}
+
+// I made the password "I Love Sonja!" for the user to input as the correct password.
+// I tried to make a "if" variable statement for when the user inputs on the following words.
+// I had a hard time figuring out on how to make a input place for the user to type in the password.
